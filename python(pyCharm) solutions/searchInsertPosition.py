20d2bd3f730c8ae5ccca040b91f733541bd16ea0
Tony Braxton Tchio Ngoumeza <tngoumeza@gmail.com>
2025-04-05 23:04:58 -0400

    added soluion to searchInsertPosition.py

---
+++ python(pyCharm) solutions/searchInsertPosition.py
@@ -0,0 +1,24 @@
+#Given a sorted array of distinct integers and a target value, return the index if the target is
+#found. If not, return the index where it would be if it were inserted in order.
+#You must write an algorithm with O(log n) runtime complexity.
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        expected_index=0
+        for i in nums:
+            if i==target:
+                return nums.index(i)
+            else:
+                if i<target:
+                    expected_index=nums.index(i)+1
+        return expected_index
+#sample test case
+nums=[12,16,21]
+target=13
+sol=Solution()
+sol = sol.searchInsert(nums, target)
+print(sol)
